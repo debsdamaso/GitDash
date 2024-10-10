689ec7084d08676f31ee38dd7ab6d289127d3ba1
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-10 08:47:27 -0300

    Conectando a API do GitHub

--- src/main/java/br/com/fiap/gitdash/github/GitHubController.java
+++ src/main/java/br/com/fiap/gitdash/github/GitHubController.java
@@ -1,5 +1,7 @@
 package br.com.fiap.gitdash.github;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,13 +20,22 @@ public class GitHubController {
     }
 
     @GetMapping("/")
-    public String getUserInfo(Model model, @RegisteredOAuth2AuthorizedClient("github") OAuth2AuthorizedClient authorizedClient) {
+    public String getUserInfo(Model model, @RegisteredOAuth2AuthorizedClient("github") OAuth2AuthorizedClient authorizedClient,@AuthenticationPrincipal OAuth2User user) {
 
         String tokenValue = authorizedClient.getAccessToken().getTokenValue();
         List<RepositoryInfo> repos = gitHubService.getUserRepositories(tokenValue);
 
+        var avatar = user.getAttributes().get("avatar_url");
+        var nome = user.getAttributes().get("name");
+        var html_url = user.getAttributes().get("html_url");
+
         model.addAttribute("repos", repos);
+
+        model.addAttribute("name", nome);
+        model.addAttribute("avatar_url", avatar);
+        model.addAttribute("html_url", html_url);
 
         return "user";
     }
 }
+
